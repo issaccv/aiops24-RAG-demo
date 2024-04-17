10bcdacfe35b2c893b7f3502ffb745adb018251d
issaccv <17275050+issaccv@users.noreply.github.com>
2024-04-18 00:18:07 +0800

    add dataset

--- run.sh
+++ run.sh
@@ -19,5 +19,12 @@ bin/ollama create qwen -f modelfile
 # download the embedding model
 git clone https://www.modelscope.cn/AI-ModelScope/bge-small-zh-v1.5.git demo/BAAI/bge-small-zh-v1.5
 
+# download dataset
+git clone https://www.modelscope.cn/datasets/issaccv/aiops2024-challenge-dataset.git demo/dataset
+
+mv demo/dataset/question.jsonl demo/question.jsonl
+
+unzip demo/dataset/data.zip -d demo/
+
 # print info 
-echo "The Ollama server is running in the background. You can now play with demo!"
+echo "The Ollama server is running in the background. Please close current terminal and you can now play with demo!"
